38ae72f45d50f2b899b04697c69bf7c947c04e10
NikitaKx9 <nikitakx9@gmail.com>
2024-11-19 05:10:37 +0300

    Review update

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -6,36 +6,35 @@ const Review = () => {
       <div className="review-container">
         <div className="review-title"></div>
         <div className="review-posts">
-          <div className="review-posts-row">
-            <div className="review-post review-post-row">
-              <div className="review-post-image">
-                <img src="src/img/girl.png" alt="girl" />
-              </div>
-              <div className="review-post-text review-post-text-col">
-                <div className="review-post-text-author">
-                  Екатерина Вальнова
-                </div>
-                <p className="review-text">
-                  <blockquote>
-                    Доброжелательные подсказки на всех этапах помогут правильно
-                    заполнить поля и без затруднений купить авиа или ж/д билет,
-                    даже если вы заказываете онлайн билет впервые.
-                  </blockquote>
-                </p>
-              </div>
-              <div className="review-post-image">
-                <img src="src/img/man.png" alt="man" />
-              </div>
-              <div className="review-post-text review-post-text-col">
-                <div className="review-post-text-author">Евгений Стрыкало</div>
-                <p className="review-text">
-                  <blockquote>
-                    СМС-сопровождение до посадки Сразу после оплаты ж/д билетов
-                    и за 3 часа до отправления мы пришлем вам СМС-напоминание о
-                    поездке.
-                  </blockquote>
-                </p>
-              </div>
+          <div className="review-post">
+            <div className="review-post-image">
+              <img src="src/img/girl.png" alt="girl" />
+            </div>
+            <div className="review-post-text review-post-text-col">
+              <div className="review-post-text-author">Екатерина Вальнова</div>
+              <p className="review-text">
+                <blockquote>
+                  Доброжелательные подсказки на всех этапах помогут правильно
+                  заполнить поля и без затруднений купить авиа или ж/д билет,
+                  даже если вы заказываете онлайн билет впервые.
+                </blockquote>
+              </p>
+            </div>
+          </div>
+
+          <div className="review-post">
+            <div className="review-post-image">
+              <img src="src/img/man.png" alt="man" />
+            </div>
+            <div className="review-post-text review-post-text-col">
+              <div className="review-post-text-author">Евгений Стрыкало</div>
+              <p className="review-text">
+                <blockquote>
+                  СМС-сопровождение до посадки Сразу после оплаты ж/д билетов и
+                  за 3 часа до отправления мы пришлем вам СМС-напоминание о
+                  поездке.
+                </blockquote>
+              </p>
             </div>
           </div>
         </div>
