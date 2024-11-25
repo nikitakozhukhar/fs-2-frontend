b8c58bd3b02bb6df2fd7fb040e45da0fdd827a10
NikitaKx9 <nikitakx9@gmail.com>
2024-11-25 06:41:30 +0300

    FilterWidget added

---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,14 @@
+import './CheckBox.css'
+
+const CheckBox = () => {
+  return (
+    <div className="checkbox-wrapper-3">
+            <input type="checkbox" id="cbx-3" />
+            <label htmlFor="cbx-3" className="toggle">
+              <span></span>
+            </label>
+          </div>
+  );
+};
+
+export default CheckBox;
--- src/pages/TrainViewPage.tsx
+++ src/pages/TrainViewPage.tsx
@@ -1,14 +1,16 @@
 import Header from "../components/Header/Header";
-import ProgressPageBar from "../components/ProgressPageBar/ProgressPageBar"
+import ProgressPageBar from "../components/ProgressPageBar/ProgressPageBar";
+import FilterWidget from "../components/FilterWidget/FilterWidget";
 
 const TrainViewPage = () => {
   return (
     <>
-      <Header location={'train'} text={false}/>
+      <Header location={"train"} text={false} />
       <ProgressPageBar />
-      
+      <div className="train-container">
+        <FilterWidget />
+      </div>
     </>
-    
   );
 };
 
