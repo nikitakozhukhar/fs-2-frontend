c4816cf2c3dc87e898ddf6b40bcb04775110a0d6
NikitaKx9 <nikitakx9@gmail.com>
2024-11-28 07:03:41 +0300

    TrainPreview added

--- src/pages/PlaceViewPage.tsx
+++ src/pages/PlaceViewPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import ProgressPageBar from "../components/ProgressPageBar/ProgressPageBar";
 import FilterWidget from "../components/FilterWidget/FilterWidget";
+import TrainPreview from "../components/TrainPreview/TrainPreview";
 
 const PlaceViewPage = () => {
   return (
@@ -11,7 +12,9 @@ const PlaceViewPage = () => {
         <div className="mr-10">
           <FilterWidget />
         </div>
-        
+        <div className="">
+          <TrainPreview />
+        </div>
       </div>
     </>
   );
