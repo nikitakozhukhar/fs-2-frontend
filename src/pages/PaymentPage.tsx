2873c5faf493da8e526b25c63b182c96faaaa48d
NikitaKx9 <nikitakx9@gmail.com>
2024-12-02 07:16:47 +0300

    Payment added

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,10 +1,22 @@
-// import React from 'react';
+import Header from "../components/Header/Header";
+import ProgressPageBar from "../components/ProgressPageBar/ProgressPageBar";
+import TripDetails from "../components/TripDetails/TripDetails";
+import Payment from "../components/Payment/Payment";
 
 const PaymentPage = () => {
   return (
-    <div>
-      PaymentPage
+    <>
+    <Header location={"train"} text={false} />
+    <ProgressPageBar />
+    <div className="flex w-[1440px] mx-auto">
+      <div className="mr-10">
+        <TripDetails />
+      </div>
+      <div className="">
+       <Payment />
+      </div>
     </div>
+  </>
   );
 };
 
