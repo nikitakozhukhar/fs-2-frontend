79da8b7f517bfd603fca6f9316c15d965a246ac6
NikitaKx9 <nikitakx9@gmail.com>
2024-12-08 19:05:47 +0300

    About switched to tailwind

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,21 +1,19 @@
-import "./About.css";
-
 const About = () => {
   return (
-    <section id="about" className="about">
-      <div className="about-title">О Нас</div>
-      <div className="about-text">
-        <p>
+    <section id="about" className="my-20 mx-auto">
+      <div className="uppercase text-base font-medium mb-5">О Нас</div>
+      <div className="w-[800px] border-l-8 border-[#ffa800] pl-3">
+        <p className="mb-5">
           Мы рады видеть вас! Мы рботаем для Вас с 2003 года. 14 лет мы
           наблюдаем, как с каждым днем все больше людей заказывают жд билеты
           через интернет.
         </p>
-        <p>
+        <p className="mb-5">
           Сегодня можно заказать железнодорожные билеты онлайн всего в 2 клика,
           но стоит ли это делать? Мы расскажем о преимуществах заказа через
           интернет.
         </p>
-        <p className="about-text__bold">
+        <p className="font-semibold">
           Покупать жд билеты дешево можно за 90 суток до отправления поезда.
           Благодаря динамическому ценообразованию цена на билеты в это время
           самая низкая.
