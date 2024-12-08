780155f96bd19e508cd48d88ee089c063af1f156
NikitaKx9 <nikitakx9@gmail.com>
2024-12-08 18:58:32 +0300

    header switched to tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
-// import React from 'react';
-import "./Header.css";
+
 import Logo from "../Logo/Logo";
 import Banner from "../Banner/Banner";
 import FindTicket from "../FindTicket/FindTicket";
@@ -28,21 +27,18 @@ const Header: React.FC<HeaderProps> = ({
     imgBannerSrc = successImage;
   }
 
-  // const imgBannerSrc = location === "train" ? trainImage : defaultImage;
-
   return (
     <header id="header" className="h-[985px]">
       <Logo />
-      <div className="absolute w-[100vw] h-[95px] left-0 bg-[#292929] -z-10"></div>
-      <div className="flex justify-start items-center h-[95px] max-w-[1440px] m-auto">
-        <nav className="">
+      <div className="absolute w-[100vw] h-[95px] left-0 bg-[#292929] -z-10 md:h-[75px]"></div>
+      <div className="flex justify-start items-center h-[95px] max-w-[1440px] m-auto md:h-[75px]">
+        <nav className="flex md:text-xs md:gap-4">
           <ul className="flex gap-[100px] text-white text-2xl">
             <li className="font-extralight cursor-pointer">
               <Link to="about" smooth={true} duration={500}>
                 О нас
               </Link>
             </li>
-
             <li className="font-extralight cursor-pointer">
               <Link to="howItWorks" smooth={true} duration={500}>
                 Как это работает
@@ -62,11 +58,11 @@ const Header: React.FC<HeaderProps> = ({
         </nav>
       </div>
 
-      <div className="header-col1">
+      <div className="flex justify-center items-center mt-[247px] ml-[150px]">
         {text && (
-          <div className="text-container">
-            <h2 className="header-text">Вся жизнь -</h2>
-            <h2 className="header-text-second">путешествие!</h2>
+          <div className="flex flex-col items-start 2xl:relative 2xl:-left-[4vw]">
+            <h2 className="font-extralight text-6xl text-white">Вся жизнь -</h2>
+            <h2 className="font-semibold text-6xl text-white">путешествие!</h2>
           </div>
         )}
 
