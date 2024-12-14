0ebbb86d95727e6b752a734ed6447a2d2f428f71
NikitaKx9 <nikitakx9@gmail.com>
2024-12-14 22:20:14 +0300

    some updates

--- src/utils/api/fetchRoutes.ts
+++ src/utils/api/fetchRoutes.ts
@@ -1,6 +1,6 @@
 import axiosInstance from "../axiosInstance/axiosInstance";
 
-interface IRoute {
+interface ITrain {
   have_first_class: boolean;
   have_second_class: boolean;
   have_third_class: boolean;
@@ -9,13 +9,84 @@ interface IRoute {
   have_air_conditioning: boolean;
   is_express: boolean;
   min_price: number;
-  arrival: object;
-  departure: object;
-  total_avaliable_seats: number;
+  train: {
+    name: string;
+    _id: string;
+  };
+  from: {
+    datetime: number;
+    railway_station_name:string;
+    city: {
+      name: string;
+      _id: string;
+    };
+  }
+  to:{
+    datetime: number;
+    railway_station_name:string;
+    city: {
+      name: string;
+      _id: string;
+    };
+  }
+  duration: number;
+  price_info: IPriceInfo;
+  seats_info?: number;
+  _id: string;
+}
+
+interface ISeatsInfo {
+  first?: number;
+  second?: number;
+  third?: number;
+  fourth?: number;
+}
+
+interface IPriceInfo {
+  first?: {
+    bottom_price?: number;
+    side_price?: number;
+    top_price?: number;
+  };
+  second?: {
+    bottom_price?: number;
+    side_price?: number;
+    top_price?: number;
+  };
+  third?: {
+    bottom_price?: number;
+    side_price?: number;
+    top_price?: number;
+  };
+  fourth?: {
+    bottom_price?: number;
+    side_price?: number;
+    top_price?: number;
+  };
+}
+
+export interface IRoute {
+  items: [
+    {
+      have_first_class: boolean;
+      have_second_class: boolean;
+      have_third_class: boolean;
+      have_fourth_class: boolean;
+      have_wifi: boolean;
+      have_air_conditioning: boolean;
+      is_express: boolean;
+      min_price: number;
+      arrival?: object;
+      departure: ITrain;
+      available_seats_info: ISeatsInfo;
+      total_avaliable_seats: number;
+    }
+  ];
+  
 }
 
 export const fetchRoutes = async (from: string, to: string): Promise<IRoute> => {
   const { data } = await axiosInstance.get<IRoute>(`routes?from_city_id=${from}&to_city_id=${to}`);
-
+  
   return data
 }
