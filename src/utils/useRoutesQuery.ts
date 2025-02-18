172b23494c03ed67370971cbbd014d48a73219a5
NikitaKx9 <nikitakx9@gmail.com>
2025-02-18 18:51:19 +0300

    renamed parametrs

--- src/utils/useRoutesQuery.ts
+++ src/utils/useRoutesQuery.ts
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchRoutes } from "./api/fetchRoutes";
 
 export const useRoutesQuery = (
-  from_city_id: string, 
-  to_city_id: string, 
+  fromCityId: string, 
+  toCityId: string, 
   limits: number, 
   offset: number,
   sort: string) => {
   return useQuery({
-    queryKey:['routes', from_city_id, to_city_id, limits, offset, sort],
-    queryFn: () => fetchRoutes(from_city_id, to_city_id, limits, offset, sort),
-    enabled: !!from_city_id && !!to_city_id
+    queryKey:['routes', fromCityId, toCityId, limits, offset, sort],
+    queryFn: () => fetchRoutes(fromCityId, toCityId, limits, offset, sort),
+    enabled: !!fromCityId && !!toCityId
   })
 }
