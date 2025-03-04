4b3fefab93a5c3881c211f3dfdb59e5ec87c032e
NikitaKx9 <nikitakx9@gmail.com>
2025-03-04 11:45:19 +0300

    Formik lib install, update Passanger form

--- src/components/Passanger/Passanger.tsx
+++ src/components/Passanger/Passanger.tsx
@@ -1,37 +1,43 @@
 import { Link } from "react-router-dom";
+import OpenedCircle from "../../img/svg/openedCircle.svg?react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import usePersonInfoStore from "../../store/personInfoStore";
 
 const Passanger = () => {
+
+  const { personData, setPersonData } = usePersonInfoStore();
+  
+  const validationSchema = Yup.object({
+    category: Yup.string().required(""),
+    firstName: Yup.string().required(""),
+    secondName: Yup.string().required(""),
+    patronymic: Yup.string().required(""),
+    gender: Yup.string().required(""),
+    birthDate: Yup.date().required(""),
+    mobility: Yup.boolean(),
+    documentType: Yup.string().required(""),
+    documentData: Yup.object({
+      series: Yup.string().required(""),
+      number: Yup.string().required(""),
+    }),
+  });
+
+  const formik = useFormik({
+    initialValues: personData,
+    validationSchema,
+    onSubmit: (values) => {
+      setPersonData(values);
+    },
+  });
+
   return (
     <div className="flex flex-col w-[960px] ">
       <div className="mb-10 border-2 border-b-4 border-gray-400">
         <div className="flex justify-between items-center py-8 px-8 bg-[#F9F9F9]">
-          <div className="flex items-center gap-5">
-            <div className="">
-              <svg
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="16"
-                  cy="16"
-                  r="15"
-                  stroke="#928F94"
-                  stroke-width="2"
-                />
-                <line
-                  x1="8"
-                  y1="16"
-                  x2="24"
-                  y2="16"
-                  stroke="#928F94"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+          <div className="flex items-center gap-5 ">
+            <div className=" cursor-pointer">
+              <OpenedCircle />
             </div>
             <div className="text-2xl font-medium">Пассажир 1</div>
           </div>
@@ -41,9 +47,15 @@ const Passanger = () => {
         <span className="w-[100%] h-[1px] bg-[#928F94]"></span>
 
         <div className="px-4 py-8">
-          <form action="" className="flex flex-col gap-8">
+          <form onSubmit={formik.handleSubmit} className="flex flex-col gap-8">
             <div className="relative w-[280px]">
-              <select className="w-full h-[50px] p-2 text-xl border-2 border-gray-400 rounded-lg appearance-none cursor-pointer outline-none bg-white pr-12">
+              <select
+                name="category"
+                value={formik.values.category}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className="w-full h-[50px] p-2 text-xl border-2 border-gray-400 rounded-lg appearance-none cursor-pointer outline-none bg-white pr-12"
+              >
                 <option value="adult">Взрослый</option>
                 <option value="child">Ребенок</option>
               </select>
@@ -60,14 +72,17 @@ const Passanger = () => {
             <div className="flex justify-between">
               <div className="flex flex-col w-[280px]">
                 <label
-                  htmlFor="second_name"
+                  htmlFor="secondName"
                   className="mb-2 text-base font-normal text-gray-700"
                 >
                   Фамилия
                 </label>
                 <input
-                  id="second_name"
-                  name="second_name"
+                  id="secondName"
+                  name="secondName"
+                  value={formik.values.secondName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
                   placeholder="Введите фамилию"
                 />
@@ -75,14 +90,17 @@ const Passanger = () => {
 
               <div className="flex flex-col w-[280px]">
                 <label
-                  htmlFor="name"
+                  htmlFor="firstName"
                   className="mb-2 text-base font-normal text-gray-700"
                 >
                   Имя
                 </label>
                 <input
-                  id="name"
-                  name="name"
+                  id="firstName"
+                  name="firstName"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
                   placeholder="Введите имя"
                 />
@@ -90,14 +108,17 @@ const Passanger = () => {
 
               <div className="flex flex-col w-[280px]">
                 <label
-                  htmlFor="third_name"
+                  htmlFor="patronymic"
                   className="mb-2 text-base font-normal text-gray-700"
                 >
                   Отчество
                 </label>
                 <input
-                  id="third_name"
-                  name="third_name"
+                  id="patronymic"
+                  name="patronymic"
+                  value={formik.values.patronymic}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
                   placeholder="Введите отчество"
                 />
@@ -108,32 +129,38 @@ const Passanger = () => {
               {/* Пол */}
               <div className="flex flex-col gap-2 w-[204px] ">
                 <div className="text-lg font-medium text-gray-700">Пол</div>
-                <div className="flex justify-around h-[50px] p-2 border-2 border-gray-400 rounded-lg ">
-                  <div className="">
-                    <label className="flex items-center gap-2 cursor-pointer">
+                <div className="flex h-[50px] justify-center items-center border-2 border-gray-400 rounded-lg">
+                  <div className="h-full flex-1 border-r-2 border-slate-700">
+                    <label className="h-full flex items-center justify-center cursor-pointer">
                       <input
                         type="radio"
                         name="gender"
                         value="male"
+                        checked={formik.values.gender === "male"}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         className="hidden peer"
                       />
-                      <div className="flex items-center justify-center  peer-checked:bg-orange-500">
-                        <span className="text-3xl text-black-700 font-bold ">
+                      <div className="h-full w-full flex items-center justify-center peer-checked:bg-orange-500">
+                        <span className="text-3xl text-black-700 font-bold">
                           М
                         </span>
                       </div>
                     </label>
                   </div>
 
-                  <div className="">
-                    <label className="flex items-center gap-2 cursor-pointer">
+                  <div className="h-full flex-1">
+                    <label className="h-full flex items-center justify-center cursor-pointer">
                       <input
                         type="radio"
                         name="gender"
                         value="female"
+                        checked={formik.values.gender === "female"}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         className="hidden peer"
                       />
-                      <div className="flex items-center justify-center  peer-checked:bg-orange-500">
+                      <div className="h-full w-full flex items-center justify-center peer-checked:bg-orange-500">
                         <span className="text-3xl text-black-700 font-bold">
                           Ж
                         </span>
@@ -144,7 +171,7 @@ const Passanger = () => {
               </div>
 
               {/* Дата рождения */}
-              <div className="flex flex-col gap-2 w-[240px]">
+              <div className="flex flex-col gap-2 w-[240px] ">
                 <label
                   htmlFor="birth_date"
                   className="text-lg font-medium text-gray-700"
@@ -153,21 +180,38 @@ const Passanger = () => {
                 </label>
                 <input
                   type="date"
-                  id="birth_date"
+                  id="birthDate"
+                  name="birthDate"
+                  value={formik.values.birthDate}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   className="h-[50px] p-2 border-2 border-gray-400 rounded-lg outline-none text-gray-600 focus:border-blue-500 cursor-pointer"
                 />
               </div>
             </div>
 
             <div className="flex items-center gap-4">
-              <input type="checkbox" id="mobility" className="hidden peer" />
+              <input
+                type="checkbox"
+                id="mobility"
+                className="hidden"
+                checked={formik.values.mobility}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
               <label
                 htmlFor="mobility"
-                className="w-8 h-8 border-2 border-gray-500 rounded-lg flex items-center justify-center cursor-pointer peer-checked:bg-blue-500 peer-checked:border-blue-500"
+                className={`w-8 h-8 border-2 border-gray-500 rounded-lg flex items-center justify-center cursor-pointer transition-colors duration-200 ${
+                  formik.values.mobility
+                    ? "text-orange-500 border-orange-500"
+                    : ""
+                }`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 text-white opacity-0 peer-checked:opacity-100 transition-opacity"
+                  className={`w-5 h-5 text-orange transition-opacity duration-200 ${
+                    formik.values.mobility ? "opacity-100" : "opacity-0"
+                  }`}
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
@@ -180,86 +224,100 @@ const Passanger = () => {
               </label>
               <label
                 htmlFor="mobility"
-                className="text-gray-700 cursor-pointer"
+                className={`${
+                  formik.values.mobility ? "text-orange-500 " : "text-gray-700"
+                } cursor-pointer select-none transition-text duration-200`}
               >
                 ограниченная подвижность
               </label>
             </div>
+            <span className="w-[100%] h-[1px] bg-[#928F94]"></span>
+
+            <div className="flex gap-8 px-4 py-8">
+              <div className="flex flex-col justify-between">
+                <label
+                  htmlFor="documentType"
+                  className="text-base font-normal text-gray-400"
+                >
+                  Тип документа
+                </label>
+
+                <div className="relative w-[205px]">
+                  <select
+                    id="documentType"
+                    name="documentType"
+                    value={formik.values.documentType}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="w-full h-[50px] p-2 text-xl border-2 border-gray-400 rounded-lg appearance-none cursor-pointer outline-none bg-white pr-12"
+                  >
+                    <option value="passport">Паспорт РФ</option>
+                    <option value="Birth_certificate">
+                      Свидетельство о рождении
+                    </option>
+                  </select>
+                  <svg
+                    className="absolute top-1/2 right-4 -translate-y-1/2 pointer-events-none"
+                    width="12"
+                    height="6"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0 0L6 6L12 0H0Z" fill="#928F94" />
+                  </svg>
+                </div>
+              </div>
+
+              <div className="flex flex-col justify-between gap-3">
+                <div className="flex flex-col w-[205px]">
+                  <label
+                    htmlFor="series"
+                    className="mb-2 text-base font-normal text-gray-400"
+                  >
+                    Серия
+                  </label>
+                  <input
+                    id="series"
+                    name="documentData.series"
+                    value={formik.values.documentData.series}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
+                    placeholder=" __   __   __   __"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col justify-between gap-3">
+                <div className="flex flex-col w-[280px]">
+                  <label
+                    htmlFor="number"
+                    className="mb-2 text-base font-normal text-gray-400"
+                  >
+                    Номер
+                  </label>
+                  <input
+                    id="number"
+                    name="documentData.number"
+                    value={formik.values.documentData.number}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
+                    placeholder="  __   __   __   __   __   __"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <span className="w-[100%] h-[1px] bg-[#928F94]"></span>
+
+            <div className="flex justify-end px-4 py-8">
+              <button
+                type="submit"
+                className="px-10 py-2 border-[1px] border-[#292929] rounded-lg text-2xl font-bold"
+              >
+                Следующий пассажир
+              </button>
+            </div>
           </form>
-        </div>
-
-        <span className="w-[100%] h-[1px] bg-[#928F94]"></span>
-
-        <div className="flex gap-8 px-4 py-8">
-          <div className="flex flex-col justify-between">
-            <label
-              htmlFor="document"
-              className="text-base font-normal text-gray-400"
-            >
-              Тип документа
-            </label>
-
-            <div className="relative w-[205px]">
-              <select
-                id="document"
-                className="w-full h-[50px] p-2 text-xl border-2 border-gray-400 rounded-lg appearance-none cursor-pointer outline-none bg-white pr-12"
-              >
-                <option value="passport">Паспорт РФ</option>
-                <option value="Birth_certificate">
-                  Свидетельство о рождении
-                </option>
-              </select>
-              <svg
-                className="absolute top-1/2 right-4 -translate-y-1/2 pointer-events-none"
-                width="12"
-                height="6"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0L6 6L12 0H0Z" fill="#928F94" />
-              </svg>
-            </div>
-          </div>
-
-          <div className="flex flex-col justify-between gap-3">
-            <div className="flex flex-col w-[205px]">
-              <label
-                htmlFor="series"
-                className="mb-2 text-base font-normal text-gray-400"
-              >
-                Серия
-              </label>
-              <input
-                id="series"
-                name="series"
-                className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
-                placeholder=" __   __   __   __"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col justify-between gap-3">
-            <div className="flex flex-col w-[280px]">
-              <label
-                htmlFor="number"
-                className="mb-2 text-base font-normal text-gray-400"
-              >
-                Номер
-              </label>
-              <input
-                id="number"
-                name="number"
-                className="h-[50px] p-2 border-2 border-gray-400 rounded-lg appearance-none outline-none text-xl"
-                placeholder="  __   __   __   __   __   __"
-              />
-            </div>
-          </div>
-        </div>
-
-        <span className="w-[100%] h-[1px] bg-[#928F94]"></span>
-
-        <div className="flex justify-end px-4 py-8">
-          <button className="px-10 py-2 border-[1px] border-[#292929] rounded-lg text-2xl font-bold">
-            Следующий пассажир
-          </button>
         </div>
       </div>
 
