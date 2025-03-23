f5d034079c338d0ae33a3ebf927bd7bb98db0bc9
NikitaKx9 <nikitakx9@gmail.com>
2025-03-23 09:59:35 +0300

    update SuccessInfoBanner

--- src/components/SuccessInfoBanner/SuccessInfoBanner.tsx
+++ src/components/SuccessInfoBanner/SuccessInfoBanner.tsx
@@ -4,7 +4,36 @@ import TicketIcon from "../../img/svg/printTicket.svg?react";
 import CheckConductor from "../../img/svg/checkConductor.svg?react";
 import Star from "../../img/svg/Star.svg?react";
 
+import orderStore from "../../store/orderStore";
+
+
+
 const SuccessInfoBanner = () => {
+
+  const { user } = orderStore();
+
+  // const handleClick = (item) => {
+  //   console.log(item)
+  // }
+
+  const starsArr = [
+    <div className="hover:text-white cursor-pointer"><Star key={1} /></div>,
+    <div className="hover:text-white cursor-pointer"><Star key={2} /></div>,
+    <div className="hover:text-white cursor-pointer"><Star key={3} /></div>,
+    <div className="hover:text-white cursor-pointer"><Star key={4} /></div>,
+    <div className="hover:text-white cursor-pointer"><Star key={5} /></div>
+  ]
+ 
+
+  // const renderStars = () => {
+  //   const stars = [];
+
+  //   for (let i = 0; i <= 4; i++) {
+  //     stars.push(<div className="hover:text-white cursor-pointer"><Star key={i} /></div>);
+  //   }
+  //   return stars;
+  // };
+
   return (
     <div className="flex flex-col flex-grow w-[1400px] mx-auto relative -top-52">
       <h1 className="text-7xl font-bold text-white mb-5">
@@ -22,7 +51,7 @@ const SuccessInfoBanner = () => {
         <div className="flex flex-col justify-center items-center gap-14 w-[1100px] mx-auto">
           <div className="flex justify-center items-center gap-11">
             <div className="flex flex-col items-center">
-              <div className="">
+              <div className="text-[#ffc655]">
                 <EmailSendIcon />
               </div>
               <div className="w-[120px] text-center text-lg">
@@ -30,7 +59,7 @@ const SuccessInfoBanner = () => {
               </div>
             </div>
             <div className="flex flex-col items-center">
-              <div className="">
+              <div className="text-[#ffc655]">
                 <TicketIcon />
               </div>
               <div className="w-[195px] text-center text-lg">
@@ -38,7 +67,7 @@ const SuccessInfoBanner = () => {
               </div>
             </div>
             <div className="flex flex-col items-center">
-              <div className="">
+              <div className="text-[#ffc655]">
                 <CheckConductor />
               </div>
               <div className="w-[205px] text-center text-lg">
@@ -47,7 +76,7 @@ const SuccessInfoBanner = () => {
             </div>
           </div>
           <div className="flex flex-col justify-start self-start mb-44">
-            <h2 className="mb-5 text-4xl font-bold">Ирина Эдуардовна!</h2>
+            <h2 className="mb-5 text-4xl font-bold">{`${user.firstName} ${user.patronymic}`}!</h2>
             <p className="text-2xl">Ваш заказ успешно оформлен.</p>
             <p className="mb-8 text-2xl">
               В ближайшее время с вами свяжется наш оператор для подтверждения.
@@ -58,16 +87,12 @@ const SuccessInfoBanner = () => {
             </p>
           </div>
         </div>
-        <div className="flex justify-between p-8 bg-orange-500">
+        <div className="flex justify-between p-8 bg-[#ffb31f]">
           <div className="flex items-center gap-5 text-2xl">
-            оценить сервис
-            <span className="flex gap-4 cursor-pointer">
-              <Star />
-              <Star />
-              <Star />
-              <Star />
-              <Star />
-            </span>
+            Оценить сервис
+            {starsArr.map(star => (
+              star
+            ))}
           </div>
 
           <Link to={"/"}>
