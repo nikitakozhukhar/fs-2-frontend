773224635fc08d88681507c8f343dd70c71b0ca1
NikitaKx9 <nikitakx9@gmail.com>
2025-03-30 15:27:09 +0300

    fetchOrder added

---
+++ src/utils/api/fetchOrder.ts
@@ -0,0 +1,20 @@
+import axiosInstance from "../axiosInstance/axiosInstance";
+import { User, Departure} from "../../store/orderStore"
+
+export interface IOrder {
+  user: User;
+  departure: Departure;
+}
+
+export const fetchOrder = async (data: IOrder) => {
+  if (!data) return [];
+
+  const orderNumber = await axiosInstance.post(`/order`, {
+    data
+  })
+  .then(response => console.log(response))
+  
+  return orderNumber;
+};
+
+export default fetchOrder
