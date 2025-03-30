c4b36d23275c9e6ce3f278f2972bcd252482f24a
NikitaKx9 <nikitakx9@gmail.com>
2025-03-30 15:26:56 +0300

    full commented

--- src/features/СitiesQuery.tsx
+++ src/features/СitiesQuery.tsx
@@ -1,39 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
 
-export const getCities = async (term: string) => {
-  try {
-    const response = await axios.get(
-      `https://students.netoservices.ru/fe-diplom/routes/cities?name=${encodeURIComponent(term)}`
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error('Ошибка при получении данных');
-  }
-};
+// export const getCities = async (term: string) => {
+//   try {
+//     const response = await axios.get(
+//       `https://students.netoservices.ru/fe-diplom/routes/cities?name=${encodeURIComponent(term)}`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     throw new Error('Ошибка при получении данных');
+//   }
+// };
 
-// Компонент для отображения списка городов
-const CitiesQuery = ({ term }: { term: string }) => {
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['cities', term],
-    queryFn: () => getCities(term),
-  });
+// // Компонент для отображения списка городов
+// const CitiesQuery = ({ term }: { term: string }) => {
+//   const { isLoading, isError, data, error } = useQuery({
+//     queryKey: ['cities', term],
+//     queryFn: () => getCities(term),
+//   });
 
-  if (isLoading) {
-    return <div>Загрузка...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Загрузка...</div>;
+//   }
 
-  if (isError) {
-    return <div>Ошибка: {error.message}</div>;
-  }
+//   if (isError) {
+//     return <div>Ошибка: {error.message}</div>;
+//   }
 
-  return (
-    <div>
-      {data?.map((city: any) => (
-        <div key={city.id}>{city.name}</div>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {data?.map((city: any) => (
+//         <div key={city.id}>{city.name}</div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default CitiesQuery;
+// export default CitiesQuery;
