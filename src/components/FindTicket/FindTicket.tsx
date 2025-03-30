b5df56b9fbfb9b900757d033f8dadcdb07653198
NikitaKx9 <nikitakx9@gmail.com>
2025-03-30 17:53:36 +0300

    update style

--- src/components/FindTicket/FindTicket.tsx
+++ src/components/FindTicket/FindTicket.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSearchDirectionStore } from "../../store/searchDirectionStore";
 import { useCitiesQuery } from "../../utils/useCitiesQuery";
-// import CustomDatePicker from "../CustomDatePicker/CustomDatePicker";
+
+import ReplaceIcon from "../../img/svg/update-alt-svgrepo-com.svg?react";
 
 const FindTicket: React.FC = () => {
   const {
@@ -27,15 +28,11 @@ const FindTicket: React.FC = () => {
     error: fromError,
   } = useCitiesQuery(fromCityLocal);
 
-  // console.log('startDateGlobal ->', startDateGlobal)
-
   const {
     data: toCities,
     isLoading: toLoading,
     error: toError,
   } = useCitiesQuery(toCityLocal);
-
- 
 
   const handleCitySelect = (
     city: { name: string; _id: string },
@@ -50,13 +47,11 @@ const FindTicket: React.FC = () => {
 
   const handleInputFromCity = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFromCityLocal(e.target.value)
-    // setFromCity(fromCities);
     setShowFromDropdown(true);
   };
 
   const handleInputToCity = (e: React.ChangeEvent<HTMLInputElement>) => {
     setToCityLocal(e.target.value)
-    // setToCity(toCities);
     setShowToDropdown(true);
   };
 
@@ -116,16 +111,7 @@ const FindTicket: React.FC = () => {
               )}
             </div>
             <div className="relative top-1 left-[6px] bg-transparent cursor-pointer">
-              <svg
-                fill="gray"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-                id="update-alt"
-                className="w-8 h-8"
-              >
-                <path d="M12,3A9,9,0,0,0,6,5.32V3A1,1,0,0,0,4,3V8a1,1,0,0,0,.92,1H10a1,1,0,0,0,0-2H7.11A7,7,0,0,1,19,12a1,1,0,0,0,2,0A9,9,0,0,0,12,3Z"></path>
-                <path d="M19.08,15H14a1,1,0,0,0,0,2h2.89A7,7,0,0,1,5,12a1,1,0,0,0-2,0,9,9,0,0,0,15,6.68V21a1,1,0,0,0,2,0V16A1,1,0,0,0,19.08,15Z"></path>
-              </svg>
+              <ReplaceIcon className="text-gray-500"/>
             </div>
             <div className="relative w-full">
               <input
@@ -185,6 +171,7 @@ const FindTicket: React.FC = () => {
               className="w-full h-12 pl-4 text-base text-gray-800 rounded-md focus:outline-none cursor-pointer"
               onChange={(e) => setStartDateGlobal(e.target.value)}
             />
+            <div className="w-[40px]"></div>
             <input
               type="date"
               name="endDate"
@@ -193,8 +180,6 @@ const FindTicket: React.FC = () => {
               onChange={(e) => setEndDateGlobal(e.target.value)}
             />
           </form>
-
-         
         </div>
 
          {/* Кнопка */}
@@ -205,7 +190,6 @@ const FindTicket: React.FC = () => {
             Найти билеты
           </button>
         </Link>
-        
       </div>
     </div>
   );
