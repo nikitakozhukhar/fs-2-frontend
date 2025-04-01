5cf966b4abc1bbb8cb22447beda5337d080a2c04
NikitaKx9 <nikitakx9@gmail.com>
2025-04-01 20:02:19 +0300

    update style

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,8 @@
 const About = () => {
   return (
     <section id="about" className="container my-20 mx-auto">
-      <div className="uppercase text-base font-medium mb-5">О Нас</div>
-      <div className="w-[800px] border-l-8 border-[#ffa800] pl-3">
+      <div className="uppercase font-medium mb-5 text-4xl">О Нас</div>
+      <div className="border-l-8 border-[#ffa800] pl-3 text-2xl">
         <p className="mb-5">
           Мы рады видеть вас! Мы рботаем для Вас с 2003 года. 14 лет мы
           наблюдаем, как с каждым днем все больше людей заказывают жд билеты
